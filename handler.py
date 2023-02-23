7fffdcfb0bee3c15d0cd28e6f2bb2f6f01c54b80
DraX <drax@MacBook-Pro-Kozloff.local>
2023-02-23 14:21:59 -0800

    Port bot to AWS + webhook infrastructure

---
+++ handler.py
@@ -0,0 +1,26 @@
+import asyncio
+import json
+import logging
+
+from main import init
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+loop = asyncio.get_event_loop()
+
+def hello(event, _):
+    try:
+        logger.info(event)
+        update = json.loads(event["body"])
+
+        bot_manager = init()
+        loop.run_until_complete(bot_manager.process_update(update))
+
+        return { "statusCode": 200 }
+                
+    except Exception as e:
+        logger.error(e)
+
+        return { "statusCode": 500 }
+
--- main.py
+++ main.py
@@ -9,33 +9,36 @@ from managers.bot import BotManager
 from managers.session import SessionManager, SessionType
 from managers.users import UsersManager
 
-logging.basicConfig(
-    format='%(asctime)s %(levelname)s [%(name)s:%(funcName)s]: %(message)s',
-    level=logging.INFO
-)
 
-google_spreadsheet_manager = GoogleSpreadsheetManager(
-    account_dict=GOOGLE_API_ACCOUNT,
-    spreadsheet_id=GOOGLE_SPREADSHEET_ID,
-    spreadsheet_user_columns=GOOGLE_SPREADSHEET_USER_COLUMNS,
-    spreadsheet_first_data_row=GOOGLE_SPREADSHEET_FIRST_DATA_ROW
-)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-session_manager = SessionManager(session_ttls={
-    SessionType.SELECT_USER: SELECT_USER_SESSION_TTL,
-    SessionType.CONFIRM_REQUEST: CONFIRM_REQUEST_SESSION_TTL
-})
+def init():
+    google_spreadsheet_manager = GoogleSpreadsheetManager(
+        account_dict=GOOGLE_API_ACCOUNT,
+        spreadsheet_id=GOOGLE_SPREADSHEET_ID,
+        spreadsheet_user_columns=GOOGLE_SPREADSHEET_USER_COLUMNS,
+        spreadsheet_first_data_row=GOOGLE_SPREADSHEET_FIRST_DATA_ROW
+    )
 
-karma_manager = KarmaManager(google_spreadsheet_manager=google_spreadsheet_manager)
-users_manager = UsersManager(users=TELEGRAM_USERS)
+    session_manager = SessionManager(session_ttls={
+        SessionType.SELECT_USER: SELECT_USER_SESSION_TTL,
+        SessionType.CONFIRM_REQUEST: CONFIRM_REQUEST_SESSION_TTL
+    })
 
-bot_manager = BotManager(
-    token=TELEGRAM_BOT_TOKEN,
-    karma_manager=karma_manager,
-    session_manager=session_manager,
-    users_manager=users_manager,
-    chat_id=TELEGRAM_CHAT_ID
-)
+    karma_manager = KarmaManager(google_spreadsheet_manager=google_spreadsheet_manager)
+    users_manager = UsersManager(users=TELEGRAM_USERS)
+
+    bot_manager = BotManager(
+        token=TELEGRAM_BOT_TOKEN,
+        karma_manager=karma_manager,
+        session_manager=session_manager,
+        users_manager=users_manager,
+        chat_id=TELEGRAM_CHAT_ID
+    )
+
+    return bot_manager
 
 if __name__ == '__main__':
+    bot_manager = init()
     bot_manager.run()
